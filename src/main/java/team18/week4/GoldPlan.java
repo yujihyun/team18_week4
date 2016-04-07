0053e2e64200b579c19a25c169448e553d899480
JH <JongHun@MacBook-Air.local>
2016-04-07 15:53:29 +0900

    데릭토리 변경

---
+++ src/main/java/team18/week4/GoldPlan.java
@@ -0,0 +1,15 @@
+package team18.week4;
+
+public class GoldPlan extends Plan{
+
+	public GoldPlan(){
+		planRate = 49.95;
+		additionalLineRate = 14.5;
+		baseMinute = 1000;
+		excessMinuteRate = 0.45;
+		familyDiscountLine = 5;
+		familyDiscountLineRate = 5;	
+	}
+	
+	
+}
