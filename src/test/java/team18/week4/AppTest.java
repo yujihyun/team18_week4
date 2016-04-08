c939b73b37a85c2b1b35bee0510c35f25bb8b6b7
JH <JongHun@MacBook-Air.local>
2016-04-08 16:20:50 +0900

    Test Code 동작 확인

--- src/test/java/team18/week4/AppTest.java
+++ src/test/java/team18/week4/AppTest.java
@@ -1,38 +1,19 @@
 package team18.week4;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import org.junit.Test;
 
 /**
- * Unit test for simple App.
+ * Created by tchi on 2016. 4. 4..
  */
-public class AppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
-
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
-
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
+public class AppTest {
+    @Test
+    public void testCalculator() {
+    	
+    	Calculator testGoldPlan = new Calculator("gold", 1010, 3);
+    	Calculator testSilverPlan = new Calculator("silver", 999, 6);
+    	
+    	testGoldPlan.calculate();
+    	testSilverPlan.calculate();
+    	
     }
 }
